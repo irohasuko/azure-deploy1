b94670db9db9ccdf844e0e79b4ef67560371e3ce
irohasuko <tottu1104@gmail.com>
2025-01-27 23:33:37 +0900

    init

---
+++ src/main/java/com/irohasuko/azure1/AzureDeploy1Application.java
@@ -0,0 +1,13 @@
+package com.irohasuko.azure1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureDeploy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDeploy1Application.class, args);
+	}
+
+}
