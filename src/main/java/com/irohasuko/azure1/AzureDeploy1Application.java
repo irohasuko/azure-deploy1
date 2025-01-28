cbb6637044fd82ceb20c3005b3ffa8658804f40a
irohasuko <tottu1104@gmail.com>
2025-01-29 07:51:34 +0900

    ServletInitializerを継承するように変更

--- src/main/java/com/irohasuko/azure1/AzureDeploy1Application.java
+++ src/main/java/com/irohasuko/azure1/AzureDeploy1Application.java
@@ -2,10 +2,17 @@ package com.irohasuko.azure1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AzureDeploy1Application {
+public class AzureDeploy1Application extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AzureDeploy1Application.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AzureDeploy1Application.class, args);
 	}
